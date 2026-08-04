43e34806b9c92406ded359eb899d2c764f2da97e
mario-bordignon <m.bordignon@aluno.ifsp.edu.br>
2026-08-04 19:36:48 -0300

    Semana-2

--- hello.py
+++ hello.py
@@ -1,41 +1,31 @@
-# A very simple Flask Hello World app for you to get started with...
-from flask import Flask, request, make_response, redirect, abort
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
+
 app = Flask(__name__)
+
+# Iniciando extensões
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 # RAÍZ
 @app.route('/')
-def hello_world():
-    return '<p>Alterações por meio do PythonAnyWhere -> GitHub</p><table><tr><td><b>Professor:</b></td><td>Professor Fabio Teixeira</td></tr><tr><td><b>Prontuário:</b></td><td>PT23820X</td></tr></table>'
+def index():
+    return render_template('index.html', current_time = datetime.utcnow())
 
-# VARIÁVEL DE NOME
+# IDENTIFICAÇÃO
 @app.route('/user/<name>')
-def user(name):
-    return '<h1>Olá, {}!</h1>'.format(name)
+@app.route('/user/<name>/<prontuario>')
+@app.route('/user/<name>/<prontuario>/<instituicao>')
+def user(name, prontuario='PT3036413', instituicao='IFSP'):
+    return render_template('id.html', name=name, prontuario=prontuario, instituicao=instituicao)
 
 # REQUISIÇÃO DE CONTEXTO
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    user_agent = request.headers.get('User-Agent')
-    return '<p>Seu navegador é {}'.format(user_agent)
+@app.route('/contextorequisicao/<name>')
+def contextorequisicao(name):
+    navegador = request.headers.get('User-Agent')
+    ip_cliente = request.remote_addr
+    host_app = request.host
+    return render_template('contexto.html', name=name, navegador=navegador, ip_cliente=ip_cliente, host_app=host_app)
 
-# CÓDIGO STATUS DIFERENTE DO PADRÃO (200)
-@app.route('/codigostatusdiferente')
-def codigostatusdiferente():
-    return '<h1>Bad Request</h1>', 400
-
-# OBJETO RESPOSTA (Como cookies)
-@app.route('/objetoresposta')
-def objetoresposta():
-    resposta = make_response('<h1>Documento com Cookies!</h1>')
-    resposta.set_cookie('id_sessao', '12345')
-    return resposta
-
-# REDIRECIONAMENTO
-@app.route('/redirecionamento')
-def redirecionamento():
-    return redirect('https://ptb.ifsp.edu.br/')
-
-# ABORT 404
-@app.route('/abortar')
-def abortar():
-    abort(404)
