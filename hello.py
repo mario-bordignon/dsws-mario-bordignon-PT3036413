391bcf9a1e2303685b19cb3afe01a32145752fcd
mario-bordignon <m.bordignon@aluno.ifsp.edu.br>
2026-08-12 09:46:45 -0300

    fix(Semana 2): correção dos códigos

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request
+from datetime import datetime
+from flask import Flask, render_template, request, make_response, redirect, abort
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
 
 app = Flask(__name__)
 
@@ -29,3 +29,18 @@ def contextorequisicao(name):
     host_app = request.host
     return render_template('contexto.html', name=name, navegador=navegador, ip_cliente=ip_cliente, host_app=host_app)
 
+# ERRO 404
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+# ERRO 500 - SERVIDOR
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+# Ativação local
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    
