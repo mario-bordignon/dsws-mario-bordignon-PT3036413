b76f293538b3116be1074f806d6fcaad3e14f377
mario-bordignon <m.bordignon@aluno.ifsp.edu.br>
2026-09-02 15:29:46 -0300

    feat: previa aula-60

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Desenvolvimento web com flask
 
-# Preparando o ambiente
+## Preparando o ambiente
 -> Criar Venv: python -m venv venv
 -> work on venv: .\venv\Scripts\Activate.ps1
 -> Baixar arquivos: pip install -r requirements/common.txt
 
-# Criando DB
+## Criando DB
 -> variável de acesso: $env:FLASK_APP = "hello.py"
     • flask shell
     • db.create_all()
     • exit()
 
-# Iniciando localmente
+## Iniciando localmente
 -> flask --app hello run --debug
--- hello.py
+++ hello.py
@@ -1,149 +1,170 @@
+# --- IMPORTAÇÕES DO FLASK ---
+# region
+
+# --- Bibliotecas padrão Python ---
+import os
 from datetime import datetime
+
+# --- Núcleo Flask ---
 from flask import Flask, render_template, request, session, redirect, url_for, flash
+
+# --- Interface e utilidades ---
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 
+# --- Formulários e Validações ---
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, SelectField, PasswordField
 from wtforms.validators import DataRequired
 
-import os
+# --- Banco de dados ---
 from flask_sqlalchemy import SQLAlchemy
 
+# endregion
+
+
+
+
+
+# --- CONFIGURAÇÕES BÁSICAS ---
+# region
+
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 app = Flask(__name__)
-
-# Chave ultra secreta
-app.config['SECRET_KEY'] = 'chaveultrasecreta'
-
+app.config['SECRET_KEY'] = 'chaveultrasecreta' # Segurança contra CSRF
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
-
-# region CLASSES
-# Classe do formulário
-class InfoForm(FlaskForm):
-    nome_completo = StringField('Informe o seu nome completo:', validators=[DataRequired()])
-    instituicao = StringField('Informe a sua instituição de ensino:', validators=[DataRequired()])
-    
-    disciplina = SelectField('Informe a sua disciplina:', choices=[
-        ('', 'Escolha uma opção'),
-        ('DWBC', 'DWBC'),
-        ('PABD', 'PABD'), 
-        ('DSWS', 'DSWS')
-    ],
-        validators=[DataRequired(message="Por favor, selecione uma disciplina válida na lista.")]
-    )
-    submit = SubmitField('Submit')
-
-# Classe de Login Forms
-class LoginForm(FlaskForm):
-    usuario = StringField('Usuário ou e-mail', validators=[DataRequired()])
-    senha = PasswordField('Informe a sua senha', validators=[DataRequired()])
-    submit = SubmitField('Enviar')
 # endregion
 
-# Iniciando extensões
+
+
+
+
+# --- INICIANDO EXTENSÕES ---
+# region
+
+db = SQLAlchemy(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-# Modelo de Cargos/Papéis
+# endregion
+
+
+
+
+
+# --- CLASSES WTFORMS ---
+# region
+
+class NameForm(FlaskForm):
+    name = StringField('Qual é o seu nome?', validators=[DataRequired()])
+    submit = SubmitField('Enviar')
+
+#endregion
+
+
+
+
+
+# --- MODELOS DB SQLALCHEMY ---
+# region
+
+# --- Cargos ---
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-    
-    # Relacionamento: Uma Role pode estar ligada a vários Users
-    users = db.relationship('User', backref='role', lazy='dynamic')
+    users = db.relationship('User', backref='role', lazy='dynamic') # Uma Role pode estar ligada a vários Users
 
     def __repr__(self):
         return f'<Role {self.name}>'
 
-# Modelo de Usuários (Atualizado)
+# --- Usuários ---
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    
-    # Chave estrangeira apontando para o id da tabela roles
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id')) # FK apontando para o id da tabela roles
 
     def __repr__(self):
         return f'<User {self.username}>'
+    
+# endregion
 
-# Relógio global
+
+
+
+
+# --- CONTEXTOS GLOBAIS ---
+# region
+
+# --- Relógio global ---
 @app.context_processor
 def inject_time():
     return dict(current_time=datetime.utcnow())
 
-# RAÍZ
+#endregion
+
+
+
+
+
+# --- ROTAS ---
+# region
+
+# --- Rota Raíz --- 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    form = InfoForm()
-    
+    form = NameForm()
     if form.validate_on_submit():
-        # Consulta no banco de dados
-        user = User.query.filter_by(username=form.nome_completo.data).first()
-        
+        user = User.query.filter_by(username=form.name.data).first()
         if user is None:
-            user = User(username=form.nome_completo.data)
+            # Busca a role padrão 'User' no banco de dados
+            user_role = Role.query.filter_by(name='User').first()
+            
+            # Cria o novo usuário vinculando-o à role padrão
+            user = User(username=form.name.data, role=user_role)
             db.session.add(user)
             db.session.commit()
+            
             session['known'] = False
         else:
             session['known'] = True
             
-        # Atualiza a sessão
-        session['nome_completo'] = form.nome_completo.data
-        session['instituicao'] = form.instituicao.data
-        session['disciplina'] = form.disciplina.data
+        session['name'] = form.name.data
         return redirect(url_for('index'))
     
-    ip_cliente = request.remote_addr
-    host_app = request.host
-        
-    return render_template('index.html', 
-                           form=form, 
-                           nome_completo=session.get('nome_completo'),
-                           instituicao=session.get('instituicao'),
-                           disciplina=session.get('disciplina'),
-                           ip_cliente=ip_cliente,
-                           host_app=host_app,
-                           known=session.get('known', False))
-
-# LOGIN
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
+    # Consulta todos os usuários para preencher a tabela HTML
+    todos_os_usuarios = User.query.all()
     
-    if form.validate_on_submit():
-        session['usuario_login'] = form.usuario.data
-        return redirect(url_for('login_response'))
-        
-    return render_template('login.html', form=form) 
+    return render_template('index.html',
+                           form=form,
+                           nome_completo=session.get('name'),
+                           known=session.get('known', False),
+                           users=todos_os_usuarios)
+
+# endregion
 
 
-# LOGIN RESPONSE
-@app.route('/loginResponse')
-def login_response():
-    usuario = session.get('usuario_login', 'Desconhecido')
-    
-    return render_template('acesso.html', usuario=usuario)
 
-# ERRO 404
+
+
+# --- ERROR HANDLING ---
+# region
+
+# --- ERRO 404 ---
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
-# ERRO 500 - SERVIDOR
+# --- ERRO 500 - SERVIDOR ---
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
-# Ativação local
+# endregion
+
+# --- SERVIDOR LOCAL ---
 if __name__ == '__main__':
     app.run(debug=True)
-
-    
